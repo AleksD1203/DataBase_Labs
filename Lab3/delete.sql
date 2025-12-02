54108e83d002452b584e060327cfe5fbfb57b1b5
Aleksandr <smiley3223337@gmail.com>
2025-12-02 11:45:17 +0200

    delete.sql update

--- Lab3/delete.sql
+++ Lab3/delete.sql
@@ -1,17 +1,17 @@
--- 19. Видалити конкретний квиток
+-- 1. Видалити конкретний квиток
 SELECT * FROM TICKET WHERE ticket_id = 6;
 DELETE FROM TICKET WHERE ticket_id = 6;
 SELECT * FROM TICKET WHERE ticket_id = 6;
 
--- 20. Видалити тестове бронювання
+-- 2. Видалити тестове бронювання
 SELECT * FROM BOOKING WHERE booking_id = 4;
 DELETE FROM BOOKING WHERE booking_id = 4;
 SELECT * FROM BOOKING WHERE booking_id = 4;
 
--- 21. Видалити тестового клієнта
-SELECT * FROM CUSTOMER WHERE name = 'Іван Коваленко';
-DELETE FROM CUSTOMER WHERE name = 'Іван Коваленко';
-SELECT * FROM CUSTOMER WHERE name = 'Іван Коваленко';
+-- 3. Видалити тестового клієнта
+SELECT * FROM CUSTOMER WHERE name = 'Юрій Савицький';
+DELETE FROM CUSTOMER WHERE name = 'Юрій Савицький';
+SELECT * FROM CUSTOMER WHERE name = 'Юрій Савицький';
 
--- 22. Спроба видалити фільм (має видати помилку FOREIGN KEY)
+-- 4. Спроба видалити фільм (має видати помилку FOREIGN KEY)
 DELETE FROM MOVIE WHERE movie_id = 4;
