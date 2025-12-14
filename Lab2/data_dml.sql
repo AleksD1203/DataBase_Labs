c91dc25d4e8968e18532979f00fe5c3a722af5d4
AleksD1203 <smiley3223337@gmail.com>
2025-12-14 20:09:05 +0200

    Update

--- Lab2/data_dml.sql
+++ Lab2/data_dml.sql
@@ -1,40 +1,47 @@
+-- 1. Додати клієнтів (email має містити @)
 INSERT INTO CUSTOMER (name, email, phone_number) VALUES
 ('Олександр Дудник', 'aleksandr.dudnik1@example.com', '380971112233'),
-('Дмитро Нечай', 'aleksandr.dudnik2@example.com', '380504445566'),
-('Олег Алєнін', 'aleksandr.dudnik3@example.com', '380637778899');
+('Дмитро Нечай', 'dmitro.nechay@example.com', '380504445566'),
+('Олег Алєнін', 'oleg.alenin@example.com', '380637778899');
 
+-- 2. Додати зали
 INSERT INTO HALL (name, total_seats) VALUES
 ('Червоний Зал', 50),
 ('Синій Зал', 75),
 ('VIP Зал', 20);
 
+-- 3. Додати фільми (Жанри мають бути зі списку: Фантастика, Драма, Бойовик...)
 INSERT INTO MOVIE (title, release_date, duration, genre) VALUES
 ('Аватар: Шлях води', '2022-12-15', 192, 'Фантастика'),
 ('Хижак: Дикі Землі', '2025-11-07', 107, 'Драма'),
-('Месники:Війна Нескінечності', '2018-04-23', 149, 'Бойовик');
+('Месники: Фінал', '2019-04-26', 181, 'Бойовик');
 
+-- 4. Додати місця (для Червоного залу, ID=1)
 INSERT INTO SEAT (seat_id, hall_id, row_number, seat_number) VALUES
 (1, 1, 1, 'A1'),
 (2, 1, 1, 'A2'),
 (3, 1, 2, 'B1'),
-(4, 1, 2, 'B2'),
-(1, 2, 1, 'C1'),
-(2, 2, 1, 'C2'),
-(3, 2, 2, 'D1');
+(4, 1, 2, 'B2');
 
+-- Додати місця (для Синього залу, ID=2)
+INSERT INTO SEAT (seat_id, hall_id, row_number, seat_number) VALUES
+(1, 2, 1, 'C1'),
+(2, 2, 1, 'C2');
+
+-- 5. Додати сеанси
 INSERT INTO SESSION (movie_id, hall_id, session_time) VALUES
 (1, 1, '2025-12-01 18:00:00'),
 (2, 2, '2025-12-01 20:30:00'),
-(1, 1, '2025-12-02 16:00:00');
+(1, 1, '2025-12-02 16:00:00'); 
 
+-- 6. Додати бронювання
 INSERT INTO BOOKING (customer_id, booking_date, total_amount) VALUES
 (1, '2025-11-02', 250.00),
-(2, '2025-11-06', 300.50),
-(3, '2025-11-10', 125.00);
+(2, '2025-11-06', 300.50);
 
+-- 7. Додати квитки
 INSERT INTO TICKET (booking_id, session_id, hall_id, seat_id, price) VALUES
 (1, 1, 1, 1, 125.00),
 (1, 1, 1, 2, 125.00),
 (2, 2, 2, 1, 150.25),
-(2, 2, 2, 2, 150.25),
-(3, 3, 1, 3, 125.00);
+(2, 2, 2, 2, 150.25);
--- Lab2/schema_ddl.sql
+++ Lab2/schema_ddl.sql
@@ -1,13 +1,15 @@
 CREATE TABLE IF NOT EXISTS CUSTOMER (
     customer_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    -- Валідація: перевірка наявності "собачки" в пошті
+    email VARCHAR(100) NOT NULL UNIQUE CHECK (email LIKE '%@%'),
     phone_number VARCHAR(15) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS HALL (
     hall_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
+    -- Валідація: зала не може мати 0 або від'ємну кількість місць
     total_seats INT NOT NULL CHECK (total_seats > 0)
 );
 
@@ -16,7 +18,8 @@ CREATE TABLE IF NOT EXISTS MOVIE (
     title VARCHAR(200) NOT NULL,
     release_date DATE NOT NULL,
     duration INT NOT NULL CHECK (duration > 0),
-    genre VARCHAR(50) NOT NULL
+    -- -- Валідація: дозволяє вводити лише жанри зі списку
+    genre VARCHAR(50) NOT NULL CHECK (genre IN ('Фантастика', 'Драма', 'Бойовик', 'Комедія', 'Жахи', 'Пригоди', 'Мультфільм'))
 );
 
 CREATE TABLE IF NOT EXISTS SEAT (
@@ -24,6 +27,8 @@ CREATE TABLE IF NOT EXISTS SEAT (
     hall_id INT NOT NULL REFERENCES HALL(hall_id) ON DELETE CASCADE,
     row_number INT NOT NULL CHECK (row_number > 0),
     seat_number VARCHAR(10) NOT NULL,
+    
+    -- Унікальність місця лише в межах залу
     PRIMARY KEY (seat_id, hall_id),
     UNIQUE (hall_id, seat_number)
 );
@@ -33,23 +38,35 @@ CREATE TABLE IF NOT EXISTS SESSION (
     movie_id INT NOT NULL REFERENCES MOVIE(movie_id) ON DELETE RESTRICT,
     hall_id INT NOT NULL REFERENCES HALL(hall_id) ON DELETE RESTRICT,
     session_time TIMESTAMP NOT NULL,
-    UNIQUE (hall_id, session_time)
+    
+    -- В одному залі в один час - один сеанс
+    UNIQUE (hall_id, session_time),
+    
+    -- Ключ для зв'язку з квитком
+    UNIQUE (session_id, hall_id)
 );
 
 CREATE TABLE IF NOT EXISTS BOOKING (
     booking_id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL REFERENCES CUSTOMER(customer_id) ON DELETE RESTRICT,
-    booking_date DATE NOT NULL,
+    booking_date DATE NOT NULL DEFAULT CURRENT_DATE,
     total_amount NUMERIC(8,2) NOT NULL CHECK (total_amount >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS TICKET (
     ticket_id SERIAL PRIMARY KEY,
     booking_id INT NOT NULL REFERENCES BOOKING(booking_id) ON DELETE CASCADE,
-    session_id INT NOT NULL REFERENCES SESSION(session_id) ON DELETE RESTRICT,
+    session_id INT NOT NULL,
     hall_id INT NOT NULL,
     seat_id INT NOT NULL,
-    price NUMERIC(5,2) NOT NULL CHECK (price > 0),
+    price NUMERIC(5,2) NOT NULL CHECK (price > 0), -- Ціна не може бути від'ємною
+    
+    -- Перевірка існування місця в залі
     FOREIGN KEY (seat_id, hall_id) REFERENCES SEAT(seat_id, hall_id) ON DELETE RESTRICT,
+    
+    -- Квиток продається на сеанс, який проходить в конкретному залі.
+    FOREIGN KEY (session_id, hall_id) REFERENCES SESSION(session_id, hall_id),
+
+    -- Одне місце на один сеанс продається лише раз
     UNIQUE (session_id, seat_id, hall_id)
 );
